d531d0e7f1bf1dba0e263c30057184c0b4a13dc5
ppoojara <ppoojara@LCS-215-027>
2023-04-21 16:04:28 -0400

    Add project files.

---
+++ BestU_v0.0.1/ToDo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestU_v0._0._1
+{
+
+    // ToDo Class
+    // This is the base class for the two other types of todo's - Habits and Single ToDo's
+    public class ToDo
+    {
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public ToDo(string _name)
+        {
+            Name = _name;
+            IsCompleted = false;
+        }
+    }
+}
